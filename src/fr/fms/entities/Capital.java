01fd5508af335f01166456d1eec811f289035415
Arthur Gibert <arthurgibert@free.fr>
2024-01-10 10:55:28 +0100

    Ajout de la classe Capital avec ces constructeurs, accesseurs et méthodes, mise en place de l'héritage depuis la classe City

---
+++ src/fr/fms/entities/Capital.java
@@ -0,0 +1,33 @@
+package fr.fms.entities;
+
+import fr.fms.entities.*;
+
+public class Capital extends City {
+	// Attributs ( tous les attributs de City ) + :
+	String monument;		// Monument
+	
+	// Constructeur :
+	public Capital(String cityName, String country, int nbrPeople, String monument) {
+		// Utilisation de super dans le contexte de l'héritage pour se référer à la classe parente (SUPERCLASSE) :
+		// Grâce à lui, on peut accéder au membres(attributs, méthodes, constructeurs) de la classe parente (City) :
+		super(cityName, country, nbrPeople);
+		this.monument = monument;
+	}
+	
+	// Accesseur (setter et getter) :
+	public String getMonument() {
+		return this.monument;
+	}
+	
+	public void setMonument(String monument) {
+		this.monument = monument;
+	}
+	
+	// Méthode de classe :
+	// Méthode de classe permettant d'utiliser system.out.println() directement sur l'objet en question :
+	@Override
+	public String toString() {
+		return this.cityName + " , " + this.country + " , " + this.getNbrPeople() + " d'habitants monument : " + this.monument;
+	}
+	
+}
